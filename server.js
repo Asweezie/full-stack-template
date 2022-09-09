a76d560381db825cbcb635ced3d865979a9d3260
brian <brianduong321@gmail.com>
2022-09-09 02:53:21 -0700

    second commit

--- server.js
+++ server.js
@@ -3,3 +3,15 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'FireEmblem',
+    collection
+
+    MongoClient.connect(dbConnectionString, (client, error) => {
+        console.log('Connected to DB on port' + PORT)
+
+        db = client.db(dbName)
+        collection = db.connection('three-houses')
+    })
