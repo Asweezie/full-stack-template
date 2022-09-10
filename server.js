7eebfb04f596955134a2ddf6f96790865764ebd9
brian <brianduong321@gmail.com>
2022-09-09 19:16:31 -0700

    add middleware

--- server.js
+++ server.js
@@ -9,9 +9,19 @@ let db,
     dbName = 'FireEmblem',
     collection
 
-    MongoClient.connect(dbConnectionString, (client, error) => {
-        console.log('Connected to DB on port' + PORT)
+    MongoClient.connect(dbConnectionString, (error, client) => {
 
+        app.set('view engine', 'ejs')
+        app.use(express.static('public'))
+        app.use(express.urlencoded({extended:true}))
+        app.use(express.json())
+        app.use(cors())
+        
+        console.log('Connected to MongoDB')
         db = client.db(dbName)
-        collection = db.connection('three-houses')
+        collection = db.collection('three-houses')
+    })
+
+    app.listen(process.env.PORT || PORT, () => {
+        console.log('Server is running...')
     })
